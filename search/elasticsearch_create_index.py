67077f2a71e8c12b84ac682d74b056eb90b6e0a2
Ryan Flynn <parseerror@gmail.com>
2015-12-12 23:25:17 -0500

    elasticsearch conn/index create/bulk import script

---
+++ search/elasticsearch_create_index.py
@@ -0,0 +1,97 @@
+# ex: set ts=4 et:
+# -*- coding: utf-8 -*-
+
+import elasticsearch
+from elasticsearch.helpers import bulk, streaming_bulk
+# ref: https://github.com/elastic/elasticsearch-py/blob/master/example/load.py
+
+from dbconn import get_psql_conn
+from psycopg2.extras import RealDictCursor
+# "analyzer": "english"
+
+# http://stackoverflow.com/questions/15079064/how-to-setup-a-tokenizer-in-elasticsearch
+# https://www.elastic.co/guide/en/elasticsearch/reference/1.4/analysis-ngram-tokenizer.html
+
+schema = \
+{
+  'properties': {
+    'updated':        { 'type': 'long'   },
+    'merchant_sku':   { 'type': 'string' },
+    'url_host':       { 'type': 'string' },
+    'url':            { 'type': 'string' },
+    'brand':          { 'type': 'string' },
+    'name':           { 'type': 'string' },
+    'descr':          { 'type': 'string', 'index': 'not_analyzed'},
+    'in_stock':       { 'type': 'boolean'},
+    'stock_level':    { 'type': 'int'    },
+    'currency':       { 'type': 'string', 'index': 'not_analyzed'},
+    'price_min':      { 'type': 'float'  },
+    'price_max':      { 'type': 'float'  },
+    'sale_price_min': { 'type': 'float'  },
+    'sale_price_max': { 'type': 'float'  },
+    'img_urls':       { 'type': 'array', 'index': 'not_analyzed' },
+  }
+}
+
+def get_rows(conn):
+    with conn.cursor(cursor_factory=RealDictCursor) as cursor:
+        cursor.execute('''
+select
+    updated,
+    merchant_sku,
+    url_host,
+    url_canonical as url,
+    brand,
+    name,
+    descr,
+    in_stock,
+    stock_level,
+    currency,
+    price_min,
+    price_max,
+    sale_price_min,
+    sale_price_max,
+    img_urls
+from url_product
+-- limit 1000
+''')
+        for row in cursor:
+            yield dict(row)
+
+'''
+curl -XPOST 'localhost:9200/test/type1/1/_update' -d '{
+    'doc' : {
+        'name' : 'new_name'
+    },
+    'doc_as_upsert' : true
+}'
+'''
+
+if __name__ == '__main__':
+
+    conn = get_psql_conn()
+
+    es = elasticsearch.Elasticsearch(
+        ['search-es0-qjusld7s4jpxxcsy2ktlkfr42m.us-east-1.es.amazonaws.com:80']
+    )
+
+    if es.indices.exists('product'):
+        print "deleting '%s' index..." % ('product')
+        res = es.indices.delete(index='product')
+        print " response: '%s'" % (res)
+
+    print es.info()
+
+    es.create(index='product',
+              doc_type='product',
+              body=schema)
+
+    cnt = 0
+    for ok, result in streaming_bulk(es, get_rows(conn),
+                                     doc_type='product', index='product'):
+        cnt += 1
+        if cnt % 100 == 0:
+            print cnt
+
+    es.indices.refresh(index='product')
+
